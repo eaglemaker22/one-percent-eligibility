18009a3edfa29cd92c55bba295dc156df90d297f
eaglemaker22 <cstheall@yahoo.com>
2026-01-19 14:28:51 -0700

    Delete ping.js

--- ping.js
+++
@@ -1,6 +0,0 @@
-export default async (req) => {
-  return new Response(
-    JSON.stringify({ ok: true, message: "pong", ts: new Date().toISOString() }),
-    { status: 200, headers: { "Content-Type": "application/json" } }
-  );
-};
