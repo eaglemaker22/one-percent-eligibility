29beac193a452ddc94d41536a5c91bd42e04a74f
eaglemaker22 <cstheall@yahoo.com>
2026-01-19 21:45:45 -0700

    Update ping.js

--- functions/ping.js
+++ functions/ping.js
@@ -1,6 +1,19 @@
-exports.handler = async function () {
-  return {
-    statusCode: 200,
-    body: "pong"
-  };
-};
+export async function handler() {
+  try {
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ok: true,
+        hasKey: !!privateKey,
+        keyStartsWith: privateKey.slice(0, 30)
+      })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+}
